e87b55488f154fcc7224bae1062373df163aeb2e
Andres Pajo <andres@apajo.ee>
2024-06-11 23:21:14 +0300

    Added nexxessary packages && tests

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -8,7 +8,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 class Configuration implements ConfigurationInterface
 {
 
-  public function getConfigTreeBuilder()
+  public function getConfigTreeBuilder(): TreeBuilder
   {
     $treeBuilder = new TreeBuilder('apajo_multi_tenancy');
     $rootNode = $treeBuilder->getRootNode();
---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace aPajo\MultiTenancyBundle\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+  public function testUnitTest()
+  {
+    dump('testUnitTest');
+    self::assertTrue(true);
+  }
+}
